c57fa460df881f77922548cafe78cef950623908
yuan.chao <2428306489@qq.com>
2019-10-29 09:01:27 +0800

    update serve

--- src/app/public/until/webServe.ts
+++ src/app/public/until/webServe.ts
@@ -1,18 +1,25 @@
 import { Subject } from 'rxjs';
 
 const urlMap = {
+    login: { url: '/user/login', dataType: 1},
+    register: { url: '/user/register', dataType: 1},
+    updatePassword: {url: '/user/updatePassword', dataType: 1},
+    deleteUser: {url: '/user/deleteUser', dataType: 1},
     getAll: { url: '/getheros', dataType: 1 },
     getById: { url: '/gethero?id=', dataType: 1 },
 };
 
 
-export function wsSend(service: string) {
+export function wsSend(service: string, param?) {
     const subject = new Subject();
     const client2 = new XMLHttpRequest();
     if (urlMap[service].dataType) {
-        client2.open('GET', '/api' + urlMap[service].url, true);
+        param ? client2.open('GET', '/api' + urlMap[service].url + param, true) : client2.open('GET', '/api' + urlMap[service].url, true);
+        // client2.open('GET', '/api' + urlMap[service].url + param, true);
+        client2.setRequestHeader('Content-Type', 'application/json');
     } else {
         client2.open('POST', '/api' + urlMap[service].url, true);
+        client2.setRequestHeader('Content-Type', 'application/json');
     }
     client2.send();
     // tslint:disable-next-line: only-arrow-functions
