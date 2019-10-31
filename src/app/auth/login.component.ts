6e3c2241d07dca090e289f8b390932215486ec2d
yuan.chao <2428306489@qq.com>
2019-10-31 13:01:53 +0800

    update login

--- src/app/auth/login.component.ts
+++ src/app/auth/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { LoginService } from './login.service';
 import { Router } from '@angular/router';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'app-login',
@@ -25,7 +26,8 @@ export class LoginComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private loginService: LoginService,
-    private router: Router
+    private router: Router,
+    private message: NzMessageService
     ) {}
 
   ngOnInit(): void {
@@ -46,11 +48,28 @@ export class LoginComponent implements OnInit {
     const param = '?name=' + data.userName + '&password=' + data.password;
     this.loginService.logIn(param).subscribe((r) => {
       const res: any = r;
-      if (res == 0) {
-        this.
+      if (res.code == 0) {
+        console.log(r);
+        const loginInfo = {
+          Authorization: 'user',
+          name: this.validateForm.value.userName
+      };
+        this._setUserInfo(loginInfo)
+        this.router.navigate(['/emas']);
+      } else {
+        this.message.create('error', `${res.msg}`);
       }
-      console.log(r);
     });
   }
+
+  private _setStorage(key, value): void {
+    localStorage.setItem(key, value);
+}
+
+private _setUserInfo(userInfo: any): void {
+  // console.log(userInfo);
+  this._setStorage('access_token', userInfo.Authorization);
+  this._setStorage('user_name', userInfo.name);
+}
 
 }
